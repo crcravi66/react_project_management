0fdccc9492607df8a31d6c5997009607b271f79a
arun <crcravi66@gmail.com>
2024-06-15 18:09:06 +0530

    Add button,input,newproject files added them

--- Refs_Demo_Project/src/App.jsx
+++ Refs_Demo_Project/src/App.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectsSidebar from "./components/ProjsctsSlidebar.jsx";
 
 
 function App() {
+  const [projectState, setProjectState] = useState({ selctedProjectId: undefined, project: [] })
+
+  function handleStartAddProject() {
+    setProjectState(preveState => {
+      return {
+        ...preveState,
+        selctedProjectId: null
+      }
+    })
+  }
+
+  let content;
+
+  if (projectState.selctedProjectId === null) {
+    content = <NewProject />
+  } else if (projectState.selctedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+
+  }
+
   return (
-    <main className="h-screen my-8">
-      <ProjectsSidebar />
+    <main className="h-screen my-8 flex gap-8 ">
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
---
+++ Refs_Demo_Project/src/components/Input.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Input({ label, textarea, ...props }) {
+
+    const txtAreaClasses = "w-full p-1 border-b-2 rounded-sm  borer-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+
+    return <p className="flex flex-col gap-1 my-4">
+        <label className=" text-sm  font-bold uppercase text-stone-500">{label}</label>
+        {textarea ? (<textarea className={txtAreaClasses}
+            {...props} />) : (<input className={txtAreaClasses} {...props} />)}
+    </p>
+} 
