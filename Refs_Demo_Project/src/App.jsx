d92ca415b7e8fc132e55a15cdcacad21c7f14e96
arun <crcravi66@gmail.com>
2024-06-19 13:30:07 +0530

    added modal and alter by new project file

--- Refs_Demo_Project/src/App.jsx
+++ Refs_Demo_Project/src/App.jsx
@@ -5,7 +5,8 @@ import ProjectsSidebar from "./components/ProjsctsSlidebar.jsx";
 
 
 function App() {
-  const [projectState, setProjectState] = useState({ selctedProjectId: undefined, project: [] })
+
+  const [projectState, setProjectState] = useState({ selctedProjectId: undefined, projects: [] })
 
   function handleStartAddProject() {
     setProjectState(preveState => {
@@ -16,18 +17,33 @@ function App() {
     })
   }
 
+  function handleAddProject(projectData) {
+    setProjectState(prevState => {
+      const projectId = Math.random()
+      const newProject = {
+        ...projectData,
+        id: projectId
+      }
+      return {
+        ...prevState,
+        selctedProjectId: undefined,
+        projects: [...prevState.projects, newProject]
+      }
+    })
+  }
+  console.log(projectState)
+
   let content;
 
   if (projectState.selctedProjectId === null) {
-    content = <NewProject />
+    content = <NewProject onAdd={handleAddProject} />
   } else if (projectState.selctedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
-
   }
 
   return (
     <main className="h-screen my-8 flex gap-8 ">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectState.projects} />
       {content}
     </main>
   );
--- Refs_Demo_Project/src/components/Input.jsx
+++ Refs_Demo_Project/src/components/Input.jsx
@@ -1,12 +1,14 @@
+import { forwardRef } from "react"
 
-
-export default function Input({ label, textarea, ...props }) {
+const Input = forwardRef(function Input({ label, textarea, ...props }, ref) {
 
     const txtAreaClasses = "w-full p-1 border-b-2 rounded-sm  borer-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
 
     return <p className="flex flex-col gap-1 my-4">
         <label className=" text-sm  font-bold uppercase text-stone-500">{label}</label>
-        {textarea ? (<textarea className={txtAreaClasses}
-            {...props} />) : (<input className={txtAreaClasses} {...props} />)}
+        {textarea ? (<textarea ref={ref} className={txtAreaClasses}
+            {...props} />) : (<input ref={ref} className={txtAreaClasses} {...props} />)}
     </p>
-} 
+});
+
+export default Input
